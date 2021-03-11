Implement Reimage and Recurrent Non-Standard Models * Due to a git mishap the following other commits got bundled into this commit: ** Implement Image Normalization ** Fix Image Resizing ** Add Model Selection to Command Line Arguments

src/image_utils.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def __open_jpg(file_path: str, image_size: Tuple[int, int, int]) -> np.ndarray:
     ret = cv2.imread(file_path)
-    ret = ret.resize(image_size)
+    ret = cv2.resize(ret, image_size[:2])
     return ret
 
 
@@ -24,12 +24,13 @@ def load_batch(directory_path: str, num_to_load: int, image_size: Tuple[int, int
     """
     files = os.listdir(directory_path)
     selected_files = np.random.choice(files, num_to_load)
-    ret_labels = np.zeros((num_to_load, 5), dtype=np.int32)
-    ret_images = np.zeros((num_to_load, *image_size, 3))
+    ret_labels = np.zeros((num_to_load, 5), dtype=np.float32)
+    ret_images = np.zeros((num_to_load, image_size[1], image_size[0], 3))
     for index, file in enumerate(selected_files):
         file_path = directory_path + sep + file
         disease_code_index = int(file.split('_')[1].split('.')[0])
         ret_labels[index] = np.zeros(5)
         ret_labels[index][disease_code_index] = 1
         ret_images[index] = __open_jpg(file_path, (*image_size, 3))
+        ret_images[index] /= 255
     return ret_images, ret_labels